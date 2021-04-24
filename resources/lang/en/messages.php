b64122e045f24d63f9577bbc13c09e26ceb87a80
Emil Mammadov <emill.memmedovv@gmail.com>
2021-04-24 22:32:07 +0400

    initialization

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "success_add" => "Added success",
+    "success_update" => "Updated success",
+    "success_delete" => "Deleted success",
+    "success" => "Finished success",
+];
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['prefix'=>'v1'], function (){
+    Route::resource('users',\App\Http\Controllers\UserController::class)->only(['create','edit','destroy']);
+});
